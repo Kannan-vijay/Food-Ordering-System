30d17a2aa195e8c84052c4bcb9d3b5330a71cda1
Kannan-vijay <kannanvijay7339099916@gmail.com>
2025-05-14 22:37:40 +0530

    initial setup

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Home from './pages/Home';
+
+function App() {
+  return (<>
+            <Home />
+            <div class="container bootstrap-dark">
+              <button class="btn btn-primary">Dark Mode Button</button>
+            </div>  
+          </>
+  );
+}
+
+export default App;
